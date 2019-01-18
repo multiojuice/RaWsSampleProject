85fb9048a45a3bbd44e1f9fb93605189d5f108c4
Owen Sullivan <multiojuice@gmail.com>
2019-01-17 21:52:13 -0500

    Importing the Raws framework and using it for hello world

--- src/main/java/com/multiojuice/RaWsSampleProject/App.java
+++ src/main/java/com/multiojuice/RaWsSampleProject/App.java
@@ -1,13 +1,14 @@
 package com.multiojuice.RaWsSampleProject;
 
+import com.multiojuice.RaWsFramework.RaWsApp;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ) {
+        RaWsApp app = new RaWsApp();
+        app.printSomething();
     }
 }
